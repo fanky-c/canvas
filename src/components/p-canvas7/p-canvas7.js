d803daa7df62e47eff85634f5ba9c6d3ef95952e
赵超 <396850524@qq.com>
2017-02-15 10:19:23 +0800

    add

--- src/components/p-canvas7/p-canvas7.js
+++ src/components/p-canvas7/p-canvas7.js
@@ -15,7 +15,7 @@ require(['util','canvasArror','canvasBall','canvasShip'],function(util, arror, b
     //定义小球
     for(var i=0; i<nums; i++){
        var size = Math.random() * 20 + 5;
-       //我们知道hex颜色值是从#000000到#ffffff(parseInt("0xffffff",16).toString(10) == 16777215)
+       //hex颜色值是从#000000到#ffffff(parseInt("0xffffff",16).toString(10) == 16777215)
        var color = '#' +Math.floor(Math.random() * 16777215).toString(16);
        var ball = new Ball(size);
        
@@ -31,7 +31,8 @@ require(['util','canvasArror','canvasBall','canvasShip'],function(util, arror, b
 
        balls.push(ball);
     }  
-
+    
+    //判断是否超过边界
     function draw(ball, pos){
         ball.x += ball.vx;
         ball.y += ball.vy;
@@ -39,7 +40,8 @@ require(['util','canvasArror','canvasBall','canvasShip'],function(util, arror, b
         if(ball.x - ball.radius > canvas.width 
            || ball.radius + ball.x < 0
            || ball.y - ball.radius > canvas.height
-           || ball.radius + ball.y < 0){
+           || ball.radius + ball.y < 0
+           ){
                 balls.splice(pos, 1);                
                 if(balls.length > 0){
                    console.log('移除：',ball.id);
@@ -60,6 +62,7 @@ require(['util','canvasArror','canvasBall','canvasShip'],function(util, arror, b
          var i = balls.length;
          while(i--){
              draw(balls[i], i);
+             //console.log(i);
          }
     })()
 })
