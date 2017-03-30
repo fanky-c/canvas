d872de4576d7f2c826e52a23a27c7dc882586b9f
zhaochao <396850524@qq.com>
2017-03-30 15:55:29 +0800

    add

--- src/components/p-canvas18/p-canvas18.js
+++ src/components/p-canvas18/p-canvas18.js
@@ -10,11 +10,10 @@ require(['util','canvasArror','canvasBall',],function(util, arror, ball){
       var spring = 0.03;
       var bounce = -1;
       
-       //创建打球
+       //创建大球
        centerBall.x = W / 2;
        centerBall.y = H / 2;
-       centerBall.radius = 60;  
-       centerBall.draw(context);
+       centerBall.radius = 60;         
        
        //创建随机小球
        for(var i=0; i<numsBalls; ++i){
@@ -25,10 +24,66 @@ require(['util','canvasArror','canvasBall',],function(util, arror, ball){
             ball.vx = Math.random() * 6 - 3;
             ball.vy = Math.random() * 6 - 3;
             balls.push(ball);
-            //ball.draw(context);
        }
 
 
-       //
+       //边界碰撞
+       function move(ball){
+             ball.x += ball.vx;
+             ball.y += ball.vy;
+
+             if(ball.x + ball.radius > canvas.width){
+                     ball.x = canvas.width - ball.x;
+                     ball.vx *= bounce;
+             }
+
+             if(ball.x + ball.radius < 0){
+                    ball.x = ball.radius;
+                    ball.vx *= bounce;
+             }
+
+             if(ball.y + ball.radius > canvas.height){
+                     ball.y = canvas.height - ball.y;
+                     ball.vy *= bounce;
+             }
+
+             if(ball.y + ball.radius < 0){
+                    ball.y = ball.radius;
+                    ball.vy *= bounce;
+             }
+
+             //ball.draw(context)               
+       }
+
+
+       //检测碰撞
+       function draw(ball){
+           var dx = ball.x - centerBall.x;
+           var dy = ball.y - centerBall.y;
+           var dist = Math.sqrt(dx*dx+dy*dy);
+           var min_dist = centerBall.radius - ball.radius;
+           
+           if(dist < min_dist){
+              var angle = Math.atan2(dy,dx);
+              var targetx = centerBall.x + Math.cos(angle) * min_dist;
+              var targetY = centerBall.y + Math.sin(angle) * min_dist;
+
+              ball.vx += (targetx - ball.x) * spring;
+              ball.vy += (targetY - ball.y) * spring;
+           }
+
+           ball.draw(context);
+
+       } 
+      
+
+       (function drawFrame(){
+               window.requestAnimationFrame(drawFrame, canvas);
+               context.clearRect(0, 0, canvas.width, canvas.height);
+
+               balls.forEach(move)
+               balls.forEach(draw)
+               centerBall.draw(context);
+       }())
       
 })
